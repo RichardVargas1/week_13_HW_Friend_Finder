90f1327060ca4ab66a05a888cd4076f05121cc3a
RichardVargas1 <richard.vargas95@gmail.com>
2018-11-11 12:49:52 -0600

    Final server.js

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-// Dependencies
-// ===========================================================
+// Setting the require for npm package "Express"
 var express = require("express");
 
-var app = express();
-var PORT = 3000;
+// Setting the the initial port
+var PORT = process.env.PORT || 8080;
 
-// Listener
-// ===========================================================
+// Set up the Express app to take care of the parsing of data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes:
+// Where I point my chosen server to a series of "route" files.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener:
+// This line of code will initiate the chosen server
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
-  
